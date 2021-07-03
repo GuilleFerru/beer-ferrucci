e133c09f353adde2d01bef0810176bec8587c1ec
GuilleFerru <guillelf@gmail.com>
2021-07-02 21:46:19 -0300

    prueba

--- src/screens/ItemDetailContainer/ItemDetailContainer.js
+++ src/screens/ItemDetailContainer/ItemDetailContainer.js
@@ -1,34 +1,55 @@
 import React, { useEffect, useState } from 'react';
+import { dataBase } from '../../Firebase/firebase';
 import { itemDetailContainerStyle } from './ItemDetailContainerStyle';
 import { ItemDetail } from './components/ItemDetail/ItemDetail';
-import { cervezaData } from '../Services/CervezaData';
 import 'poppins-font';
 import { makeStyles } from '@material-ui/core';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import { useParams, Redirect } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 
 const useStyles = makeStyles((theme) => itemDetailContainerStyle(theme));
 
-const promise = new Promise((resolve, reject) => {
-    setTimeout(() => resolve(cervezaData), 2000)
-})
+// const promise = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve(cervezaData), 2000)
+// })
 
 export const ItemDetailContainer = () => {
     const classes = useStyles();
     const [item, setItem] = useState([]);
     const { id } = useParams();
+    const [loading, setLoading] = useState(true);
+
+    // useEffect(() => {
+    //     promise.then((data) => {
+    //         const dataFiltrada = data.filter(cerveza => cerveza.id === id);
+    //         setItem(dataFiltrada)
+    //     }).catch(() => <Redirect to={'/*'} />)
+    // }, [id])
 
     useEffect(() => {
-        promise.then((data) => {
-            const dataFiltrada = data.filter(cerveza => cerveza.id === id);
-            setItem(dataFiltrada)
-        }).catch(() => <Redirect to={'/*'} />)
+        const itemCollection = dataBase.collection("cervezas");
+        const item = itemCollection.doc(id)
+        item.get().then((doc) => {
+            if (!doc.exists) {
+                return;
+            }
+            setItem([{ id: doc.id, ...doc.data() }])
+        }).catch((error) => {
+            console.log("Error getting document:", error);
+        }).finally(() => {
+            setLoading(false)
+        });
     }, [id])
+
+    // useEffect(() => {
+    //     console.log(item)
+    // }, [item])
+
 
 
     return <>
-        {item.length === 0 ? (
+        {loading ? (
             <div className={classes.preloaderContainer}>
                 <CircularProgress size='6rem' color='inherit' />
             </div>
